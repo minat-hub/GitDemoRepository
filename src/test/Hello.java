ca4d7eae787d2a7c5185b9c7b4d7f7ab53843ef2
300390465 <300390465@N5107S32.ad.douglascollege.ca>
2025-04-14 12:14:50 -0700

    testing commit

--- src/test/Hello.java
+++ src/test/Hello.java
@@ -4,7 +4,8 @@ public class Hello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("This code is by Manager");
+		System.out.println("git project");
 	}
 
 }
